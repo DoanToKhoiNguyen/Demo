eb931f6080eba5e3bfd5adfa10085b0b2424d46b
DoanToKhoiNguyen <khoinguyen1982005@gmail.com>
2026-09-08 10:14:31 +0700

    Successfull

--- Tuan01/API_Async_Await/API+Async_Await/script.js
+++ Tuan01/API_Async_Await/API+Async_Await/script.js
@@ -1,1 +1,26 @@
-const a = 10;
+const status = document.getElementById("status");
+const productsList = document.getElementById("products");
+
+async function getProducts() {
+    try {
+        const response = await fetch("https://dummyjson.com/products");
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        data.products.forEach((product) => {
+            const li = document.createElement("li");
+            li.textContent = product.title;
+            productsList.appendChild(li);
+        });
+
+        status.textContent = "Products loaded successfully!";
+    }
+    catch (error) {
+        status.textContent = `Error loading products`;
+    }
+}
+getProducts();
