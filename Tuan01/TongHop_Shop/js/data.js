e45f0cb678a9e1a40f57b70fa6089da5be275e1c
DoanToKhoiNguyen <khoinguyen1982005@gmail.com>
2026-09-12 10:39:47 +0700

    Update TongHop

--- Tuan01/TongHop_Shop/js/data.js
+++ Tuan01/TongHop_Shop/js/data.js
@@ -2,13 +2,19 @@ let products = [];
 
 async function getproducts() {
     try {
+        // fetch() gửi request đến API để lấy dữ liệu sản phẩm
+        // await: chờ API trả về kết quả rồi mới chạy tiếp
         const response = await fetch("https://fakestoreapi.com/products");
 
         if(!response.ok){
             throw new Error("Không thể lấy sản phẩm");
         }
 
+        // response.json() đọc dữ liệu JSON từ API
+        // await dùng để chờ quá trình chuyển dữ liệu hoàn tất
+        // Trả về một mảng (Array) các Object sản phẩm
         const data = await response.json();
+
         products = data.slice(0, 6);
         return products;
     } 
